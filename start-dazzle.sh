4c85ce1278314f77b5a0b4dd2f1d7a430d5a8f00
Jacknic <jacknic@qq.com>
2025-12-15 08:46:33 +0000

    调整缓存指向路径

--- start-dazzle.sh
+++ start-dazzle.sh
@@ -1,3 +1,3 @@
 sudo /usr/bin/buildkitd --debug  --config ./buildkitd.toml &
-mkdir -p /workspace/registry &
-docker run -p 5000:5000 --name registry --rm -v /workspace/registry:/var/lib/registry registry:2
+mkdir -p /tmp/registry &
+docker run -p 5000:5000 --name registry --rm -v /tmp/registry:/var/lib/registry registry:2
